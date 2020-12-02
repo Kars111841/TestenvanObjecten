72076f880e7b04a53d2ad0c054e5ec26350f069d
Kars111841 <70636223+Kars111841@users.noreply.github.com>
2020-12-02 11:13:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(1920, 1080);
+}
+  
+  function draw() {
+    //achtergrond
+    background(0, 0, 0);
+    noStroke();
+    fill(128, 128, 128);
+    rect(0, 0, 60, 1080);
+    rect(0, 0, 1920, 60);
+    rect(1860, 0, 60, 1080);
+    rect(0, 1020, 1920, 60);
+
+    
+}
